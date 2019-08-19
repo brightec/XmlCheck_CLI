3c6bc59ada7b5fd5c7d3622fa1ce27f44aba737e
Alistair Sykes <alistair@brightec.co.uk>
2019-08-19 17:41:36 +0100

    master: Added a naming convention check for the id attr

--- src/main/kotlin/uk/co/brightec/xmlcheck/rules/android/Id.kt
+++ src/main/kotlin/uk/co/brightec/xmlcheck/rules/android/Id.kt
@@ -15,6 +15,64 @@ class Id : AttrCheck() {
             return failure(attr, "Doesn't start with @+id/")
         }
 
+        val attrId = attr.value.substringAfter("@+id/")
+
+        val parentTagName = attr.ownerElement.tagName
+        if (!checkIdNamingConvention(attrId, parentTagName)) {
+            return failure(attr, "Id for $parentTagName doesn't conform to naming convention")
+        }
+
         return null
+    }
+
+    // TODO : Finish configuring checkIdNamingConvention()
+    private fun checkIdNamingConvention(attrId: String, parentTagName: String): Boolean {
+        val className = parentTagName.split(".").last()
+        val words = splitClassIntoWords(className)
+
+        // General Exceptions
+        if (className == "View" || className == "include") {
+            return true
+        }
+
+        // Specific exception
+        if (className == "HorizontalScrollView" && attrId.startsWith("scroll")) {
+            return true
+        }
+        if (className == "GifImageView" && attrId.startsWith("image")) {
+            return true
+        }
+        if (className == "Guideline" && attrId.startsWith("guide")) {
+            return true
+        }
+
+
+        // General rules
+        if (words.contains("Button") && attrId.startsWith("button")) {
+            return true
+        }
+        if (attrId.startsWith(words.first().toLowerCase())) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun splitClassIntoWords(cpt: String): List<String> {
+        val words = arrayListOf<String>()
+        var ongoingWord = ""
+        for (i in 0 until cpt.length) {
+            val c = cpt[i]
+            if (Character.isUpperCase(c)) {
+                if (ongoingWord.isNotEmpty()) {
+                    words.add(ongoingWord)
+                }
+                ongoingWord = "" + c
+            } else {
+                ongoingWord += c
+            }
+        }
+        words.add(ongoingWord)
+        return words
     }
 }
